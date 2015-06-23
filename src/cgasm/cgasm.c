a09a18002d6a90bbd7aec13f9ecab879f543b2de
shunting <zhangst1986@gmail.com>
2015-06-23 21:38:46 +0800

    identify function declaration

--- src/cgasm/cgasm.c
+++ src/cgasm/cgasm.c
@@ -32,7 +32,34 @@ static void cgasm_function_definition(struct cgasm_context *ctx, struct declarat
 	panic("ni");
 }
 
+static int is_func_decl_direct_declarator_suffix(struct direct_declarator_suffix *suff) {
+	return suff->empty_paren || suff->param_type_list != NULL;
+}
+
+static int is_func_decl_declarator(struct declarator *declarator) {
+	struct direct_declarator *direct_declarator = declarator->direct_declarator;
+	if (direct_declarator->id == NULL) {
+		return 0;
+	}
+	struct dynarr *darr = direct_declarator->suff_list;
+	return dynarr_size(darr) == 1 && is_func_decl_direct_declarator_suffix(dynarr_get(darr, 0));
+}
+
+static int is_func_decl_init_declarator_list(struct init_declarator_list *init_declarator_list) {
+	struct dynarr *darr = init_declarator_list->darr;
+	if (dynarr_size(darr) != 1) {
+		return 0;
+	}
+	struct init_declarator *init_declarator = dynarr_get(darr, 0);
+	return init_declarator->initializer == NULL && is_func_decl_declarator(init_declarator->declarator);
+}
+
 static void cgasm_declaration(struct cgasm_context *ctx, struct declaration_specifiers *decl_specifiers, struct init_declarator_list *init_declarator_list) {
+	// function declaration
+	if (is_func_decl_init_declarator_list(init_declarator_list)) {
+		return; // XXX ignore function declaration right now
+	}
+
 	panic("ni");
 }
 
