bc4296e498b02d73a6e619729932aaa906f5c9b7
shunting <zhangst1986@gmail.com>
2015-07-07 16:22:52 +0800

    handle '&'

--- src/cgasm/handle-op.c
+++ src/cgasm/handle-op.c
@@ -1,6 +1,20 @@
 #include <inc/cgasm.h>
 #include <inc/util.h>
+#include <inc/lexer.h>
+
+struct expr_val cgasm_handle_ampersand(struct cgasm_context *ctx, struct expr_val operand) {
+	if (operand.type != EXPR_VAL_SYMBOL) {
+		panic("'&' can only be applied to symbol");
+	}
+	operand.type = EXPR_VAL_SYMBOL_ADDR;
+	return operand;
+}
 
 struct expr_val cgasm_handle_unary_op(struct cgasm_context *ctx, int tok_tag, struct expr_val operand) {
-	panic("ni");
+	switch (tok_tag) {
+	case TOK_AMPERSAND:
+		return cgasm_handle_ampersand(ctx, operand);
+	default:
+		panic("ni %s", token_tag_str(tok_tag));
+	}
 }
