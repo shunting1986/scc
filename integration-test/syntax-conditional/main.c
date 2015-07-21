1037d11217600caf4b1e71836d0168d9867eb499
shunting <zhangst1986@gmail.com>
2015-07-21 20:42:58 +0800

    enhance the test case for conditional

--- integration-test/syntax-conditional/main.c
+++ integration-test/syntax-conditional/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+int check(int v) {
+	printf("check %d\n", v);
+	return v;
+}
+
 int
 main() {
 	int a, b, c, d;
 	while (scanf("%d%d%d%d", &a, &b, &c, &d) != -1) {
-		printf("%d\n", a > b ? c : d);	
+		printf("%d\n", a > b ? check(c) : check(d));	
 	}
 	return 0;
 }
