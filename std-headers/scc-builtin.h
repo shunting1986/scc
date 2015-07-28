5905e53e039352e8d097d89180d452bff12568f1
shunting <zhangst1986@gmail.com>
2015-07-28 23:46:18 +0800

    change a message

--- std-headers/scc-builtin.h
+++ std-headers/scc-builtin.h
@@ -1,6 +1,6 @@
 #define __builtin_va_list void *
 
 static void scc_builtin_abort() {
-	printf("scc does not support this feature yet");
+	printf("scc does not support this feature yet\n");
 	exit(1);
 }
