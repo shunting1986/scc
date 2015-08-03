170d05eddf5467dde9f55b8c093315e22595bcf6
shunting <zhangst1986@gmail.com>
2015-08-03 17:23:27 +0800

    add test case for int64

---
+++ integration-test/int64/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+long long add(long long a, long long b) {
+	return a + b;
+}
+
+int
+main(void) {
+	// XXX: this test case does not really check value beyond 32 bits yet, we just make sure
+	// the values within 32 bit range are handled correctly
+	long long a, b;
+	while (scanf("%lld%lld", &a, &b) != EOF) {
+		printf("%lld\n", add(a, b));
+	}
+	return 0;
+}
