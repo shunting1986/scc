52640332c2d11ec2405c4d1671556dba50431f81
shunting <zhangst1986@gmail.com>
2015-08-06 00:47:33 +0800

    support or assign

--- integration-test/arith/main.c
+++ integration-test/arith/main.c
@@ -7,12 +7,15 @@ main(void) {
 		int m = v;
 		int d = v;
 		int md = v;
+		int orv = v;
 		m *= 2;
 		printf("*=2 %d\n", m);
 		d /= 3;
 		printf("/=3 %d\n", d);
 		md %= 10;
 		printf("%%=10 %d\n", md);
+		orv |= 7;
+		printf("|=7 %d\n", orv);
 	}
 	return 0;
 }
